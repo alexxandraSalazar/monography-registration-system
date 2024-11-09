ad64ab2bbe342d2fe00a0563469fec810358d732
Alexandra Salazar <138623509+Alex000127@users.noreply.github.com>
2024-11-08 22:51:29 -0600

    feat: delete funcionality :sparkles:

--- Register/static/Register/main.js
+++ Register/static/Register/main.js
@@ -162,4 +162,190 @@ document.body.addEventListener('htmx:afterOnLoad', function(event) {
   } else {
       console.log('Form with ID "monoForm" not found!');
   }
+
+
+  const deleteMonoForm = document.getElementById("deleteMonoForm");
+  console.log(deleteMonoForm)
+  if (deleteMonoForm) {
+    console.log('here')
+      deleteMonoForm.addEventListener("submit", async function(event) {
+        console.log('no')
+        event.preventDefault()
+        console.log('btn clicked')
+          const deleteMonoUrl = deleteMonoForm.getAttribute("data-register-url");
+          const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+          const deleteMonoData = new FormData(deleteMonoForm);
+
+          try {
+              const response = await fetch(deleteMonoUrl, {
+                  method: 'POST',
+                  headers: {
+                      "X-CSRFToken": csrfToken,
+                  },
+                  body: deleteMonoData,
+              });
+
+              const dataMonoDelete = await response.json();
+              console.log("Response data:", dataMonoDelete);
+
+              if (dataMonoDelete.status === 'success') {
+                  Swal.fire({
+                      title: '¡Éxito!',
+                      text: dataMonoDelete.message,
+                      icon: 'success',
+                      confirmButtonText: 'OK',
+                  }).then(() => {
+                      const indexUrl = document.body.getAttribute('data-index-url');
+                      htmx.ajax('GET', indexUrl, { target: '#content' });
+                  });
+              } else {
+                  Swal.fire({
+                      title: 'Error!',
+                      text: dataMonoDelete.message || 'Hubo un problema. Por favor, intente nuevamente.',
+                      icon: 'error',
+                      confirmButtonText: 'OK',
+                  });
+              }
+          } catch (error) {
+              console.log("Fetch error:", error);
+              Swal.fire({
+                  title: 'Error!',
+                  text: 'Error de red. Por favor, intente nuevamente.',
+                  icon: 'error',
+                  confirmButtonText: 'OK',
+              });
+          }
+      });
+  } else {
+      console.log('Form with ID "deleteMonoForm" not found!');
+  }
+    
+  const deleteEstuForm = document.getElementById("deleteEstuForm");
+  console.log(deleteEstuForm);
+  if (deleteEstuForm) {
+      console.log('Form found');
+      deleteEstuForm.addEventListener("submit", async function(event) {
+          event.preventDefault();
+          console.log('Button clicked');
+  
+          const deleteEstuUrl = deleteEstuForm.getAttribute("data-register-url");
+          const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+          const deleteEstuData = new FormData(deleteEstuForm);
+  
+          try {
+              const response = await fetch(deleteEstuUrl, {
+                  method: 'POST',
+                  headers: {
+                      "X-CSRFToken": csrfToken,
+                  },
+                  body: deleteEstuData,
+              });
+  
+              const dataEstuDelete = await response.json();
+              console.log("Response data:", dataEstuDelete);
+  
+              if (dataEstuDelete.status === 'success') {
+                  Swal.fire({
+                      title: '¡Éxito!',
+                      text: dataEstuDelete.message,
+                      icon: 'success',
+                      confirmButtonText: 'OK',
+                  }).then(() => {
+                      // To do: move the attribute to the partial correspondant html
+                      const addEstuUrl = document.querySelector('.title').getAttribute('data-addEstu-url');
+                      console.log(addEstuUrl)
+                      if (addEstuUrl) {
+                          htmx.ajax('GET', addEstuUrl, { target: '#content' });
+                      } else {
+                          console.error("addEstuUrl not found on the body element.");
+                      }
+                  });
+              } else {
+                  Swal.fire({
+                      title: 'Error!',
+                      text: dataEstuDelete.message || 'Hubo un problema. Por favor, intente nuevamente.',
+                      icon: 'error',
+                      confirmButtonText: 'OK',
+                  });
+              }
+          } catch (error) {
+              console.log("Fetch error:", error);
+              Swal.fire({
+                  title: 'Error!',
+                  text: 'Error de red. Por favor, intente nuevamente.',
+                  icon: 'error',
+                  confirmButtonText: 'OK',
+              });
+          }
+      });
+  } else {
+      console.log('Form with ID "deleteEstuForm" not found!');
+  }
+
+  const deleteProfForm = document.getElementById("deleteProfForm");
+  console.log(deleteProfForm);
+  if (deleteProfForm) {
+      console.log('Form found');
+      deleteProfForm.addEventListener("submit", async function(event) {
+          event.preventDefault();
+          console.log('Button clicked');
+  
+          const deleteProfUrl = deleteProfForm.getAttribute("data-register-url");
+          const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+          const deleteProfData = new FormData(deleteProfForm);
+  
+          try {
+              const response = await fetch(deleteProfUrl, {
+                  method: 'POST',
+                  headers: {
+                      "X-CSRFToken": csrfToken,
+                  },
+                  body: deleteProfData,
+              });
+  
+              const dataProfDelete = await response.json();
+              console.log("Response data:", dataProfDelete);
+  
+              if (dataProfDelete.status === 'success') {
+                  Swal.fire({
+                      title: '¡Éxito!',
+                      text: dataProfDelete.message,
+                      icon: 'success',
+                      confirmButtonText: 'OK',
+                  }).then(() => {
+                      // to do: same as above
+                      const addProfUrl = document.querySelector('.title').getAttribute('data-addProf-url');
+                      console.log(addProfUrl)
+                      if (addProfUrl) {
+                          htmx.ajax('GET', addProfUrl, { target: '#content' });
+                      } else {
+                          console.error("addProfUrl not found on the body element.");
+                      }
+                  });
+              } else {
+                  Swal.fire({
+                      title: 'Error!',
+                      text: dataProfDelete.message || 'Hubo un problema. Por favor, intente nuevamente.',
+                      icon: 'error',
+                      confirmButtonText: 'OK',
+                  });
+              }
+          } catch (error) {
+              console.log("Fetch error:", error);
+              Swal.fire({
+                  title: 'Error!',
+                  text: 'Error de red. Por favor, intente nuevamente.',
+                  icon: 'error',
+                  confirmButtonText: 'OK',
+              });
+          }
+      });
+  } else {
+      console.log('Form with ID "deleteProfForm" not found!');
+  }
 });
+
+
+
+
+
--- Register/views.py
+++ Register/views.py
@@ -1,16 +1,20 @@
 from django.http import JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from .models import Monografia, Estudiante, Profesor, Rol, ProfesorMonografia
 import sweetify
 
 # Create your views here.
 def index(request):
-        if request.htmx:
-            if request.method == "GET":
-                monos = Monografia.objects.all()
-                return render(request, "partials/table-mono.html", {'monos' : monos})
-        monos = Monografia.objects.all()
-        return render(request,'Register/layout.html', {'monos' : monos})
+    if request.htmx:
+        if request.method == "GET":
+            index_url = reverse('index') 
+            monos = Monografia.objects.all()
+            return render(request, "partials/table-mono.html", {'monos': monos,  'index_url': index_url})
+
+    monos = Monografia.objects.all()
+    index_url = reverse('index') 
+    addEstuUrl = reverse('addEstu') 
+    return render(request, 'Register/layout.html', {'monos': monos, 'index_url': index_url})
 
 def registerMono(request):
     if request.method == "POST":
@@ -98,61 +102,70 @@ def registerProf(request):
     return render(request,'Register/layout.html')
     
 def addEstu(request):
+        addEstuUrl = reverse('addEstu') 
         if request.htmx:
             if request.method == "POST":
                 print('post')
-                
-                return render(request, "partials/add-estu.html")
+                return render(request, "partials/add-estu.html", {'addEstuUrl': addEstuUrl})
             elif request.method == "GET":
                 monos = Monografia.objects.all()
                 students = Estudiante.objects.all()
-                return render(request, "partials/add-estu.html",{'students': students, 'monos': monos})
-        return render(request,'Register/layout.html')
+                return render(request, "partials/add-estu.html",{'students': students, 'monos': monos, 'addEstuUrl': addEstuUrl})
+        return render(request,'Register/layout.html', {'addEstuUrl': addEstuUrl})
     
 def addTutor(request):
+        addProfUrl = reverse('addTutor')
         if request.htmx:
             if request.method == "POST":
-                return render(request, "partials/add-tutor.html")
+                return render(request, "partials/add-tutor.html", {'addProfUrl': addProfUrl})
             elif request.method == "GET":
                 profesores = Profesor.objects.all()
-                return render(request, "partials/add-tutor.html",{'profesores': profesores})
+                return render(request, "partials/add-tutor.html",{'profesores': profesores, 'addProfUrl': addProfUrl})
         return render(request,'Register/layout.html')
     
 def addJudges(request):
+        addProfUrl = reverse('addJudges')
         if request.htmx:
             if request.method == "POST":
-                return render(request, "partials/add-judges.html")
+                return render(request, "partials/add-judges.html", {'addProfUrl': addProfUrl})
             elif request.method == "GET":
                 profesores = Profesor.objects.all()
-                return render(request, "partials/add-judges.html",{'profesores': profesores})
+                return render(request, "partials/add-judges.html",{'profesores': profesores, 'addProfUrl': addProfUrl})
         return render(request,'Register/layout.html')
     
     
 def deleteMono(request, id):
-    if request.htmx:
-        if request.method == "POST":
-            monografia = Monografia.objects.get(id=id)
-            monografia.delete()
-            msg = "Monografía eliminada con éxito"
-            monos = Monografia.objects.all()  
-            return render(request, "partials/table-mono.html", {'monos': monos})
-        
+    if request.method == "POST":
+        print('POST request received')
+        try:
+            monografia = Monografia.objects.get(id=id) 
+            monografia.delete() 
+            return JsonResponse({'status': 'success', 'message': 'Monografía borrada exitosamente'}, status=200)
+            
+        except Monografia.DoesNotExist:
+            return JsonResponse({'status': 'error', 'message': 'Monografía no encontrada'}, status=404)
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': f'Error al eliminar Monografía: {str(e)}'}, status=500) 
 
 def deleteProf(request, id):
-    if request.htmx:
-        if request.method == "POST":
+    if request.method == "POST":
+        try:
             profesor = Profesor.objects.get(id=id)
             profesor.delete()
-            msg = "Profesor eliminado con éxito"
-            profesores = Profesor.objects.all()  
-            return render(request, "partials/table-mono.html",{'profesores': profesores})
-        
+            return JsonResponse({'status': 'success', 'message': 'Profesor borrado exitosamente'}, status=200)   
+        except Profesor.DoesNotExist:
+            return JsonResponse({'status': 'error', 'message': 'Profesor no encontrado'}, status=404)
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': f'Error al eliminar profesor: {str(e)}'}, status=500)
+
 def deleteEstu(request, id):
-    if request.htmx:
-        if request.method == "POST":
+    if request.method == "POST":
+        try:
             estudiante = Estudiante.objects.get(id=id)
             estudiante.delete()
-            msg = "Estudiante eliminada con éxito"
-            students = Estudiante.objects.all()  
-            return render(request, "partials/table-mono.html",{'students': students})
-
+            return JsonResponse({'status': 'success', 'message': 'Estudiante borrado exitosamente'}, status=200)   
+        except Estudiante.DoesNotExist: 
+            return JsonResponse({'status': 'error', 'message': 'Estudiante no encontrado'}, status=404)
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': f'Error al eliminar estudiante: {str(e)}'}, status=500)
+ 
